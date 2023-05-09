f723a3e92a5263b2c2375bd687e46121ab4ccf18
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2023-05-09 12:13:03 -0300

    Add files via upload

---
+++ exemplo/teste.js
@@ -0,0 +1,66 @@
+// alert("A Julia é chata :/")
+
+/*
+var nome = prompt("Digite o seu nome: ")
+console.log(nome)
+document.write(nome)
+
+var a = 10
+console.log(a)
+a = 15
+console.log(a)
+
+const a = 10
+console.log(a)
+a = 15
+console.log(a)
+
+let a = 10
+console.log(a)
+
+if(true){
+let a = 15
+console.log(a)
+   
+}
+
+console.log(a)
+
+
+//TEMPLATE STRING => INTERPOLAÇÃO
+
+var nome = prompt ("Digite o seu nome:")
+alert("Bom dia " + nome)
+
+
+alert(`Bom dia ${nome}`)
+
+*/
+
+
+
+// VARIAVEIS
+
+var n1 = parseInt(prompt("Digite o N1: "))
+var n2 = parseInt(prompt("Digite o N2: "))
+console.log("N1 é : " + typeof n1)
+console.log("N2 é : " + typeof n2)
+
+var soma = n1 + n2
+console.log("A soma é " + typeof soma)
+
+
+
+if(soma>10){
+    document.write(`<p class="maior">A soma de ${n1} com ${n2} é ${soma}</p>`)
+}
+else{
+    document.write(`<p class="menor">A soma de ${n1} com ${n2} é ${soma}</p>`)
+}
+
+
+
+
+
+
+
